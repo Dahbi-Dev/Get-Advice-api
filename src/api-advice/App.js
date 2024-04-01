878b81a7917e68728fab0c690a8699ca272c6d05
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-04-01 15:38:17 +0000

    add counter functionality logic

--- src/api-advice/App.js
+++ src/api-advice/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const App = () => {
     // State variable to hold the advice
     const [advice, setAdvice] = useState('get advice')
+    const [count, setCount] = useState(0)
 
     // Function to fetch advice from the API
     async function getAdvice() {
@@ -11,6 +12,7 @@ const App = () => {
             const res = await fetch("https://api.adviceslip.com/advice")
             const data = await res.json();
             setAdvice(data.slip.advice)
+            setCount(count+1)
         } catch (error) {
             // Handling errors during fetching
             console.error('Error fetching advice:', error);
@@ -20,13 +22,17 @@ const App = () => {
     }
 
 
+
     return (
         <div style={{ textAlign: 'center', padding: '50px', border: '3px solid #322' }}>
             <h1>Get your Daily Advices : </h1>
             <p style={{ color: 'blue', margin: '50px', fontSize: '22px' }}>{advice}</p>
             <button
-                style={{ border: '  solid 1px', cursor: 'pointer', backgroundColor: '#393', color: 'white', fontSize: '16px' }}
-                onClick={getAdvice}>Get Advice</button>
+                style={{ border: '  solid 1px', cursor: 'pointer',
+                 backgroundColor: '#393', color: 'white', fontSize: '16px' }}
+                onClick={getAdvice } >Get Advice</button>
+
+                <p>you have read <strong style={{color:'#393'}}>{count} </strong>peaces of advice <strong>Today</strong></p>
         </div>
     );
 }
