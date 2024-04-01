db187e70dca4d682a3c809868f954e1711107131
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-04-01 16:27:05 +0000

    set a loading

--- src/api-advice/App.js
+++ src/api-advice/App.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Message from './Message';
+import ReactLoading from 'react-loading';
+
+
 
 const App = () => {
     // State variable to hold the advice
     const [advice, setAdvice] = useState('get advice');
     const [count, setCount] = useState(0);
     const [buttonVisible, setButtonVisible] = useState(true);
+    const [loading, setLoading] = useState(false); // State to track loading status
+
 
     // Function to fetch advice from the API
     async function getAdvice() {
@@ -17,8 +22,10 @@ const App = () => {
             setCount((c) => c + 1);
             // Hide the button for 1500 milliseconds
             setButtonVisible(false);
+            setLoading(true)
             setTimeout(() => {
                 setButtonVisible(true);
+                setLoading(false)
             }, 1500);
         } catch (error) {
             // Handling errors during fetching
@@ -34,6 +41,15 @@ const App = () => {
         <div style={{ textAlign: 'center', padding: '50px', border: '3px solid #322' }}>
             <h1>Get your Daily Advices : </h1>
             <p style={{ color: 'blue', margin: '50px', fontSize: '22px' }}>{advice}</p>
+            {
+                loading && (
+
+                    <div  style={{paddingBottom:'30px', position: 'absolute', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                        <ReactLoading  type="spin" color="#393" height={30} width={30} />
+                    </div>
+                )
+            }
+            
             {buttonVisible && (
                 <button
                     style={{ border: '  solid 1px', cursor: 'pointer',
