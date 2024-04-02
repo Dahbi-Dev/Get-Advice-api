cd1d4e2f96e5097cd8462f2d289724043c9fc5ef
HOUSSAM DAHBI <122475783+Dahbi-Dev@users.noreply.github.com>
2024-04-02 18:32:49 +0000

    Update Message.js

--- src/api-advice/Message.js
+++ src/api-advice/Message.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Message = (props) => {
     return (
         <div>
-            <p>you have read <strong style={{ color: '#393' }}>{props.count} </strong>peaces of advice <strong>Today</strong></p>
+            <p>you have read 
+        <strong style={{ color: '#393' }}>{props.count} </strong>
+    pieces of advice <strong>Today</strong></p>
 
         </div>
     );
